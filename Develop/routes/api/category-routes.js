a5795fc73ffe636f0b1876680969ac3209dc5039
Sun <solskinartwork@gmail.com>
2021-11-28 03:56:25 -0500

    working on api routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -10,7 +10,22 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findAll({ 
+    attributes:['id', category_name],
+    include: [
+  {
+    model:Product,
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+  }
+]
+})
   // be sure to include its associated Products
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+  })
+
 });
 
 router.post('/', (req, res) => {
